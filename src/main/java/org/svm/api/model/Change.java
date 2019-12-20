75a9cf5101a5633a4a1752eb42eb3af1fccbe0c5
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-12-20 12:51:57 -0600

    adding dollars in change

--- src/main/java/org/svm/api/model/Change.java
+++ src/main/java/org/svm/api/model/Change.java
@@ -1,6 +1,7 @@
 package org.svm.api.model;
 
 public class Change {
+    private int dollars;
     private int quarters;
     private int dimes;
     private int nickels;
@@ -8,12 +9,22 @@ public class Change {
 
     public Change(Float balance) {
         pennies = Integer.parseInt(balance.toString().replace(".", ""));
+        dollars = pennies / 100;
+        pennies %= 100;
         quarters = pennies / 25;
         pennies %= 25;
         dimes = pennies / 10;
         pennies %= 10;
         nickels = pennies / 5;
         pennies %= 5;
+    }
+
+    public int getDollars() {
+        return this.dollars;
+    }
+
+    public void setDollars(int dollars) {
+        this.dollars = dollars;
     }
 
     public int getQuarters() {
