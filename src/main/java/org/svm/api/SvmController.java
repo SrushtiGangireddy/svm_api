c3a888057ecef7202ac371bfeda1ab89ac0d82f4
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-12-20 16:46:41 -0600

    adding method to get availability

--- src/main/java/org/svm/api/SvmController.java
+++ src/main/java/org/svm/api/SvmController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ public class SvmController {
     @Autowired
     private VendingService vendingService;
 
+    @CrossOrigin(origins = "https://svm-web-262622.appspot.com")
     @GetMapping("/")
     public HashMap<String, Object> home() {
         HashMap<String, Object> response = new HashMap<>();
@@ -24,6 +26,7 @@ public class SvmController {
         return response;
     }
 
+    @CrossOrigin(origins = "https://svm-web-262622.appspot.com")
     @GetMapping(value="/insertAmount/{amount}/{quantity}")
     public HashMap<String, Object> insertAmount(@PathVariable String amount, @PathVariable int quantity) {
         vendingService.insertMoney(amount, quantity);
@@ -36,6 +39,7 @@ public class SvmController {
         return response;
     }
 
+    @CrossOrigin(origins = "https://svm-web-262622.appspot.com")
     @GetMapping(value="/dispenseSoda/{sodaId}")
     public HashMap<String, Object> dispenseSoda(@PathVariable int sodaId) {
         vendingService.setSodaSelection(sodaId);
@@ -49,6 +53,7 @@ public class SvmController {
         return response;
     }
 
+    @CrossOrigin(origins = "https://svm-web-262622.appspot.com")
     @GetMapping(value="/returnChange")
     public HashMap<String, Object> returnChange() {
         vendingService.returnChange();
@@ -61,6 +66,7 @@ public class SvmController {
         return response;
     }
 
+    @CrossOrigin(origins = "https://svm-web-262622.appspot.com")
     @GetMapping(value="/selectSoda/{id}")
     public HashMap<String, Object> selectSoda(@PathVariable("id") int id) {
         vendingService.setSodaSelection(id);
@@ -73,6 +79,7 @@ public class SvmController {
         return response;
     }
 
+    @CrossOrigin(origins = "https://svm-web-262622.appspot.com")
     @GetMapping(value = "/getAvailability")
     public HashMap<String, Object> getAvailability() {
         HashMap<String, Object> response = new HashMap<>();
